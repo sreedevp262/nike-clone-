6c7ee04ee418383b06012d82074847a91d6dd005
sreedevp35@gmail.com <sreedevp35@gmail.com>
2025-03-12 12:05:43 +0530

    new 3 update

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin:'https://stately-pudding-1dbd86.netlify.app/', // Allow only your frontend URL
+  origin:'https://stately-pudding-1dbd86.netlify.app', // Allow only your frontend URL
   credentials: true, // Allow cookies and credentials
 }));
 // app.use(helmet()); // Set security headers
